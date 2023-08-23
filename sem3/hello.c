e41cd377482ac3fc7faf8802cbdfdcc2bd87ad61
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-08-23 20:41:22 +0530

    fixed

--- sem3/hello.c
+++ sem3/hello.c
@@ -1,3 +1,4 @@
+// Min-Heap data structure in C
 #include <stdio.h>
 int size = 0;
 void swap(int *a, int *b) {
@@ -42,6 +43,8 @@ void deleteRoot(int array[], int num) {
       break;
   }
 
+
+
   swap(&array[i], &array[size - 1]);
   size -= 1;
   for (int i = size / 2 - 1; i >= 0; i--) {
@@ -53,20 +56,17 @@ void printArray(int array[], int size) {
     printf("%d ", array[i]);
   printf("\n");
 }
-
 void print2dArray(int array[][2], int size) {
   printf("{");
-  for (int i = 0; i < size; i++)
+  for (int i = 0; i < size; ++i)
     printf("{%d,%d}, ", array[i][0], array[i][1]);
   printf("}\n");
 }
-
 // Main function to do heap sort
 void heapSort(int arr[], int n) {
   // Build max heap
   for (int i = n / 2 - 1; i >= 0; i--)
     heapify(arr, n, i);
-
   // Heap sort
   for (int i = n - 1; i >= 0; i--) {
     swap(&arr[0], &arr[i]);
@@ -87,41 +87,28 @@ void bubbleSort(int arr[][2], int size) {
   }
 }
 
+
+
 int main() {
 
-  // int intervels[7][2] = {{0, 2}, {1, 4},  {4, 6}, {0, 4}, {7, 8}, {9, 11},
-  // {3, 10}};
+  int intervels[7][2] = {{0, 2}, {1, 4},  {4, 6}, {0, 4},
+                         {7, 8}, {9, 11}, {3, 10}};
 
   int intervel_size = 7;
-
-  int intervels[100][2];
-
   bubbleSort(intervels, intervel_size);
 
-  printf("How many intervels:");
-  scanf("%d", &intervel_size);
-  printf("Enter intervels: (give start,end)\n");
-
-  for (int i = 0; i < intervel_size; i++) {
-    scanf("%d,%d", &intervels[i][0], &intervels[i][1]);
-  }
-
-  printf("dbg: 1");
-  int laptop_heap[10];
+  int laptop_queue[10];
   int laptop_needed = 0;
   print2dArray(intervels, intervel_size);
 
   for (int i = 0; i < intervel_size; i++) {
-    if (size >= 0 && laptop_heap[0] <= intervels[i][0]) {
-      deleteRoot(laptop_heap, laptop_heap[0]);
+    if (size > 0 && laptop_queue[0] <= intervels[i][0]) {
+      deleteRoot(laptop_queue, size);
     }
-    insert(laptop_heap, intervels[i][1]);
-
-    printf("dbg: 2");
+    insert(laptop_queue, intervels[i][1]);
     laptop_needed = laptop_needed < size ? size : laptop_needed;
   }
+  printArray(laptop_queue, size);
 
-  printArray(laptop_heap, size);
-
-  printf("laptop needed: %d\n", size);
+  printf("laptop needed: %d", laptop_needed);
 }
